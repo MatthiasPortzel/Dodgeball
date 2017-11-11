4eac739ab616ea32cdd568b72b929b09cb3d608d
Matthias <MatthiasSaihttam1@gmail.com>
2017-11-11 15:46:02 -0800

    Changes to player storage.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const Player = require('./player.js').Player;
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
@@ -11,11 +13,15 @@ http.listen(8080, function(){
     console.log('listening on *:8080');
 });
 
-io.on('connection', function(socket){
-    socket.playerID = Math.random().toFixed(8);
+var players = [];
+
+io.on('connection', function(socket) {
+    socket.playerID = players.length;
+    players.push(new Player(players.length));
 
     socket.on("username change", function (data) {
-        console.log(`Player with id ${socket.playerID} changed username to ${data}`);
+        console.log(`Player with id ${socket.playerID} changed username to ${JSON.parse(data).username}`);
+
     })
 
     console.log(socket.playerID + " connected.")
--- player.js
+++ player.js
@@ -1,3 +1,17 @@
-class Player {
+var players = [];
 
+class Player {
+    constructor(id) {
+        this.id = id;
+    }
+
+    //Username setter
+    set username(new_username) {
+        this.username = new_username;
+    }
+
+}
+
+module.exports = {
+    Player: Player,
 }
