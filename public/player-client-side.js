db525620bdc10f94920535b63c2fc1a59bdc2921
joshua16266261 <joshua6261@att.net>
2017-11-11 16:59:41 -0800

    Testing

--- public/player-client-side.js
+++ public/player-client-side.js
@@ -7,42 +7,43 @@ class player{
   setUsername(new_username) {
       this.username = new_username;
   }
+}
 
-  var movement = {
-    up: false,
-    down: false
+var movement = {
+  up: false,
+  down: false
+}
+var shoot = false;
+
+document.addEventListener('keydown', function(event){
+  switch(event.keyCode){
+    case 38: // up arrow
+      movement.up = true;
+      alert('up');
+      break;
+    case 40: // right arrow
+      movement.down = true;
+      alert('down');
+      break;
   }
-  var shoot = false;
-
-  /*document.addEventListener('keydown', function(event){
-    switch(event.keyCode){
-      case 38: // up arrow
-        movement.up = true;
-        alert('up');
-        break;
-      case 40: // right arrow
-        movement.down = true;
-        alert('down');
-        break;
-    }
-  });
-  document.addEventListener('keyup', function(event){
-    switch(event.keyCode){
-      case 38: // up arrow
-        movement.up = false;
-        break;
-      case 40: // down arrow
-        movement.down = false;
-        break;
-    }
-  });
-  document.addEventListener('keypress', function(event){
+});
+document.addEventListener('keyup', function(event){
+  switch(event.keyCode){
+    case 38: // up arrow
+      movement.up = false;
+      break;
+    case 40: // down arrow
+      movement.down = false;
+      break;
+  }
+});
+  /*document.addEventListener('keypress', function(event){
     switch(event.keyCode){
       case 32: // spacebar
         shoot = true;
         break;
     }
-  }*/
+  }
 
   document.addEventListener("keydown", move(keyCode));
 
@@ -54,4 +55,4 @@ class player{
         break;
     }
   }
-}
+}*/
