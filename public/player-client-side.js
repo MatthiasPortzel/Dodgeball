b2b8c3e86e2c0ba93edadf274920fff8c7bcf6d0
joshua16266261 <joshua6261@att.net>
2017-11-11 16:36:56 -0800

    Testing movement

--- public/player-client-side.js
+++ public/player-client-side.js
@@ -1,35 +1,45 @@
 class player{
-  var movement = {
-    left: false,
-    right: false
+  constructor(id) {
+      this.id = id;
   }
-    document.addEventListener('keydown', function(event) {
-      switch (event.keyCode) {
-        case 37: // left arrow
-          movement.left = true;
-          break;
-        case 39: // right arrow
-          movement.right = true;
-          break;
-      }
-    });
-    document.addEventListener('keyup', function(event) {
-      switch (event.keyCode) {
-        case 37: // left arrow
-          movement.left = false;
-          break;
-        case 39: // right arrow
-          movement.right = false;
-          break;
-      }
-    });
 
-    constructor(id) {
-        this.id = id;
-    }
+  //Username setter
+  setUsername(new_username) {
+      this.username = new_username;
+  }
 
-    //Username setter
-    setUsername(new_username) {
-        this.username = new_username;
+  var movement = {
+    up: false,
+    down: false
+  }
+  var shoot = false;
+
+  document.addEventListener('keydown', function(event){
+    switch(event.keyCode){
+      case 38: // up arrow
+        movement.up = true;
+        alert('up');
+        break;
+      case 40: // right arrow
+        movement.down = true;
+        break;
     }
+  });
+  document.addEventListener('keyup', function(event){
+    switch(event.keyCode){
+      case 38: // up arrow
+        movement.up = false;
+        break;
+      case 40: // down arrow
+        movement.down = false;
+        break;
+    }
+  });
+  document.addEventListener('keypress', function(event){
+    switch(event.keyCode){
+      case 32: // spacebar
+        shoot = true;
+        break;
+    }
+  }
 }
