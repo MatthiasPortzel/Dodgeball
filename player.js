eff731f6322c44737a77447d605040859719ba3e
joshua16266261 <joshua6261@att.net>
2017-11-11 16:18:25 -0800

    Adding client side stuff

--- player.js
+++ player.js
@@ -1,29 +1,4 @@
 class Player {
-  var movement = {
-    left: false,
-    right: false
-  }
-    document.addEventListener('keydown', function(event) {
-      switch (event.keyCode) {
-        case 37: // left arrow
-          movement.left = true;
-          break;
-        case 39: // right arrow
-          movement.right = true;
-          break;
-      }
-    });
-    document.addEventListener('keyup', function(event) {
-      switch (event.keyCode) {
-        case 37: // left arrow
-          movement.left = false;
-          break;
-        case 39: // right arrow
-          movement.right = false;
-          break;
-      }
-    });
-
     constructor(id) {
         this.id = id;
     }
