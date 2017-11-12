492677c1fd268547cacb4796e23abbb553ed525d
Matthias <MatthiasSaihttam1@gmail.com>
2017-11-11 16:09:40 -0800

    Resolve merge conflicts

--- player.js
+++ player.js
@@ -1,33 +1,29 @@
-<<<<<<< HEAD
 class Player {
   var movement = {
     left: false,
     right: false
   }
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 37: // left arrow
-      movement.left = true;
-      break;
-    case 39: // right arrow
-      movement.right = true;
-      break;
-  }
-});
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 37: // left arrow
-      movement.left = false;
-      break;
-    case 39: // right arrow
-      movement.right = false;
-      break;
-  }
-});
-=======
-var players = [];
+    document.addEventListener('keydown', function(event) {
+      switch (event.keyCode) {
+        case 37: // left arrow
+          movement.left = true;
+          break;
+        case 39: // right arrow
+          movement.right = true;
+          break;
+      }
+    });
+    document.addEventListener('keyup', function(event) {
+      switch (event.keyCode) {
+        case 37: // left arrow
+          movement.left = false;
+          break;
+        case 39: // right arrow
+          movement.right = false;
+          break;
+      }
+    });
 
-class Player {
     constructor(id) {
         this.id = id;
     }
@@ -36,10 +32,8 @@ class Player {
     setUsername(new_username) {
         this.username = new_username;
     }
-
 }
 
 module.exports = {
     Player: Player,
->>>>>>> 08dd1a9e6f81ba722ae1fd3188b092f2658456fd
 }
